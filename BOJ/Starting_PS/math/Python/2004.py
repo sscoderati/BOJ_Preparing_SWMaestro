bf3ac8b7c1698033bad5838897984df4956db198
sscoderati <chungup5495@gmail.com>
2021-02-06 12:18:36 +0000

    solved

--- BOJ/Starting_PS/math/Python/2004.py
+++ BOJ/Starting_PS/math/Python/2004.py
@@ -1,11 +1,25 @@
 import sys
-import itertools
+import math
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-cnt = 0
-data = list(range(1, n + 1))
 
-for x in itertools.combinations(data, m):
-    cnt += 1
-print(cnt)
+def two_Counter(n):
+    cnt = 0
+    while n != 0:
+        n = math.floor(n / 2)
+        cnt += n
+    return cnt
+
+def five_Counter(n):
+    cnt = 0
+    while n != 0:
+        n = math.floor(n / 5)
+        cnt += n
+    return cnt
+
+tcnt = two_Counter(n) - two_Counter(m) - two_Counter(n - m)
+fcnt = five_Counter(n) - five_Counter(m) - five_Counter(n - m)
+tzero = min(tcnt, fcnt)
+
+print(tzero)
