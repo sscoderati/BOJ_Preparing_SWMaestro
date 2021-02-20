f6f0646379c4f6aa7f78685c6df5321869f37343
Ubuntu <rati@felix.mrbld4w5ioxexli5lurkqa3a0e.psx.internal.cloudapp.net>
2021-02-20 12:25:44 +0000

    logic fixed

--- BOJ/Starting_PS/graph/Python/14502.py
+++ BOJ/Starting_PS/graph/Python/14502.py
@@ -1,48 +1,42 @@
 import sys
-import copy
+from itertools import combinations
+from copy import deepcopy
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-lab = [list(map(int, input().split())) for _ in range(n)]
-tmp = [[0] * m for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+lab = []
+virus = []
+space = []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    for j in range(m):
+        if tmp[j] == 2:
+            virus.append((i, j))
+        if tmp[j] == 0:
+            space.append((i, j))
+    lab.append(tmp)
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-result = 0
-
-def virus(x, y):
+def dfs(lab, y, x):
+    lab[y][x] = 2
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if tmp[nx][ny] == 0:
-                tmp[nx][ny] = 2
-                virus(nx, ny)
+        if 0 <= ny < n and 0 <= nx < m and lab[ny][nx] == 0:
+            dfs(lab, dy, dx)
+result = 0
 
-def safe_zone():
-    safe = 0
+for c in combinations(space, 3):
+    tmp_lab = deepcopy(lab)
+    for y, x in c:
+        tmp_lab[y][x] = 1
+    cnt = 0
     for i in range(n):
         for j in range(m):
-            if tmp[i][j] == 0:
-                safe += 1
-    return safe
-
-def dfs(count):
-    global result
-    if count == 3:
-        tmp = copy.deepcopy(lab)
-        for i in range(n):
-            for j in range(m):
-                if tmp[i][j] == 2:
-                    virus(i, j)
-        result = max(result, safe_zone())
-        return True
-    for i in range(n):
-        for j in range(m):
-            if lab[i][j] == 0:
-                lab[i][j] = 1
-                dfs(count + 1)
-                lab[i][j] = 0
-dfs(0)
+            if tmp_lab[i][j] == 2:
+                dfs(tmp_lab, i, j)
+    for g in tmp_lab:
+        cnt += g.count(0)
+    result = max(result, cnt)
 print(result)
