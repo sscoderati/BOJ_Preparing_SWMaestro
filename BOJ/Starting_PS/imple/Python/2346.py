7ef54a5e85b736190b622c689f462391eb4859f7
sscoderati <chungup5495@gmail.com>
2021-02-22 12:11:55 +0000

    solved

--- BOJ/Starting_PS/imple/Python/2346.py
+++ BOJ/Starting_PS/imple/Python/2346.py
@@ -1,16 +1,21 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
-
 n = int(input())
-balloon = deque(list(map(int, input().split())))
-num = deque([i for i in range(1, n + 1)])
+balloon = list(map(int, input().split()))
+
+num = [i for i in range(1, n + 1)]
 result = []
-for _ in range(n):
-    m = balloon.popleft()
-    result.append(num.popleft())
+idx = 0
+
+m = balloon.pop(idx)
+result.append(num.pop(idx))
+
+while len(balloon) > 0:
     if m > 0:
-        m -= 1
-    balloon.rotate(m)
-    num.rotate(m)
+        idx = (idx + (m - 1)) % len(balloon)
+    else:
+        m = m * -1
+        idx = len(balloon) - idx - 1
+        idx = len(balloon) - ((idx + m) % len(balloon)) - 1
+    m = balloon.pop(idx)
+    result.append(num.pop(idx))
+
 print(*result)
