ead2126e088958e5409d9e3fccf394bd28a2c5a4
sscoderati <chungup5495@gmail.com>
2021-02-22 11:51:21 +0000

    solved

--- BOJ/Starting_PS/imple/Python/2840.py
+++ BOJ/Starting_PS/imple/Python/2840.py
@@ -4,23 +4,31 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 rotation = []
+check = True
 for _ in range(k):
     a, b = map(str, input().split())
     rotation.append((int(a), b))
-wheel = deque([0 for _ in range(n)])
+wheel = deque(['?' for _ in range(n)])
 
 for i in range(k):
     r, alpha = rotation[i]
     wheel.rotate(r)
-    if wheel[0] == 0:
-        wheel[0] = alpha
-    elif wheel[0] == alpha:
-        continue
+    if wheel[0] != '?':
+        if wheel[0] == alpha:
+            continue
+        check = False
     else:
-        print("!")
-        sys.exit()
+        wheel[0] = alpha
 
-while 0 in wheel:
-    wheel[wheel.index(0)] = "?"
+for i in range(n):
+    if wheel[i] == '?':
+        continue
+    for j in range(i + 1, n):
+        if wheel[i] == wheel[j]:
+            check = False
+            break
 
-print("".join(wheel))
+if check == False:
+    print('!')
+else:
+    print("".join(wheel))
