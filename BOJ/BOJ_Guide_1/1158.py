310f3e64117b4c9bb8575c9d1409589d40d79d9d
sscoderati <chungup5495@gmail.com>
2021-02-24 08:09:00 +0000

    solution added

--- BOJ/BOJ_Guide_1/1158.py
+++ BOJ/BOJ_Guide_1/1158.py
@@ -1,3 +1,18 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+q = deque([i for i in range(1, n + 1)])
+res = []
+
+while q:
+    q.rotate(-k + 1)
+    res.append(str(q.popleft()))
+
+print("<" + ", ".join(res) + ">")
+
+"""
 from collections import deque
 
 n, k = map(int, input().split())
@@ -15,4 +30,4 @@ for i in range(n):
     else:
         print(',', end=' ')
 print('>', end ='')
-
+"""
