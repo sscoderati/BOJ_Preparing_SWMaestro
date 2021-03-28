5916af18a768a868d24d01fe60dcf0dfddf3292a
sscoderati <chungup5495@gmail.com>
2021-03-28 13:54:36 +0000

    logic fixed, but unsolved

--- BOJ/Starting_PS/brute_force/Python/1107.py
+++ BOJ/Starting_PS/brute_force/Python/1107.py
@@ -4,19 +4,14 @@ input = sys.stdin.readline
 target = int(input())
 m = int(input())
 if m == 0:
-    button = set(x for x in range(10))
+    button = set()
 else:
     malbutton = set(map(int, input().split()))
     button = set(x for x in range(10)).difference(malbutton)
 
 channel = abs(target - 100)
 for i in range(1000001):
-    flag = True
-    for j in str(i):
-        if j not in button:
-            flag = False
-            break
-    if flag:
-        channel = min(channel, abs(target - i) + len(i))
-
+    a = set(list(str(i)))
+    if not button & a:
+        channel = min(channel, len(str(i)) + abs(target - i))
 print(channel)
