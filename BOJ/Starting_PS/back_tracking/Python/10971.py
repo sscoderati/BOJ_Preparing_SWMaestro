c039119f485109ca1fd7f21111d5faa0d41f3c56
sscoderati <chungup5495@gmail.com>
2021-04-02 21:15:44 +0000

    21.04.02

---
+++ BOJ/Starting_PS/back_tracking/Python/10971.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+expense = [list(map(int, input().split())) for _ in range(n)]
+
---
+++ BOJ/Starting_PS/sort/Python/11728.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+print(*sorted(a + b))
